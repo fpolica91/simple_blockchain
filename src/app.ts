0daef1e2eec5852ef855f0b3acf095ea753ca6e4
fabricio policarpo <fabriciopolicarpo0@gmail.com>
2021-04-24 22:22:32 -0400

    implementing proof of work

--- src/app.ts
+++ src/app.ts
@@ -4,36 +4,33 @@ import 'dotenv/config';
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 
-import routes from './routes';
+// import routes from './routes';
 import AppError from './errors/AppError';
 import BlockChain from './models/Blockchain';
+import P2pserver from './models/P2pserver';
 
 // import createConnection from './database';
-
 
 const app = express();
 
 app.use(express.json());
 
-const blockchain = new BlockChain()
-import P2pserver from './models/P2pserver';
+const blockchain = new BlockChain();
 
 const p2pserver = new P2pserver(blockchain);
-p2pserver.listen()
-
+p2pserver.listen();
 
 app.get('/blocks', (request, response) => {
-  response.json(blockchain.chain)
-})
-
+  response.json(blockchain.chain);
+});
 
 app.post('/mine', (request, response) => {
-  const {data} = request.body
-  const block = blockchain.addBlock(data)
-  console.info("a new block was added s%", block.toString())
-  p2pserver.syncChain()
-  response.redirect('/blocks')
- })
+  const { data } = request.body;
+  const block = blockchain.addBlock(data);
+  console.info('a new block was added s%', block.toString());
+  p2pserver.syncChain();
+  response.redirect('/blocks');
+});
 // app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
@@ -51,6 +48,5 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
     message: 'Internal server error',
   });
 });
-
 
 export default app;
--- src/models/Block.ts
+++ src/models/Block.ts
@@ -1,14 +1,21 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-plusplus */
 import Sha256 from 'crypto-js/sha256';
 
 interface BlockData {
-  timestamp: Date;
+  timestamp: number;
   lastHash: string;
   hash: string;
   data: any;
+  nonce: number;
+  difficulty: number;
 }
 
+const DIFFICULTY = 3;
+const MINE_RATE = 100000;
+
 export default class Block {
-  timestamp: Date;
+  timestamp: number;
 
   lastHash: string;
 
@@ -16,40 +23,86 @@ export default class Block {
 
   data: any;
 
-  constructor({ timestamp, lastHash, hash, data }: BlockData) {
+  nonce: number;
+
+  difficulty: number;
+
+  constructor({
+    timestamp,
+    lastHash,
+    hash,
+    data,
+    nonce,
+    difficulty,
+  }: BlockData) {
     this.timestamp = timestamp;
     this.lastHash = lastHash;
     this.hash = hash;
     this.data = data;
+    this.nonce = nonce;
+    this.difficulty = difficulty;
   }
 
   // creates the very first block, first block always dummy
   static genesis(): Block {
     return new this({
-      timestamp: new Date(Date.now()),
+      timestamp: Date.now(),
       lastHash: '-----------',
       hash: 'genesis_hash',
       data: [],
+      nonce: 0,
+      difficulty: 2,
     });
   }
 
   // creates a new block
   static mineBlock(lasblock: Block, data: any): Block {
-    const timestamp = new Date(Date.now());
-
+    // const timestamp = Date.now();
+    //
+    // const lastHash = lasblock.hash;
+    // const hash = Block.hash(timestamp, lastHash, data);
+    // const nonce = 0;
+    let hash;
+    let timestamp;
     const lastHash = lasblock.hash;
-    const hash = Block.hash(timestamp, lastHash, data);
-    const block = new this({
-      timestamp,
-      lastHash,
-      hash,
+    let { difficulty } = lasblock;
+    let nonce = 0;
+    do {
+      nonce++;
+      timestamp = Date.now();
+      difficulty = Block.adjustDifficulty(lasblock, timestamp);
+      hash = Block.hash(timestamp, lastHash, data, nonce, difficulty);
+      // checking if we have the required no of leading number of zeros
+    } while (hash.substring(0, difficulty) !== '0'.repeat(difficulty));
+    return new this({
       data,
+      difficulty,
+      hash,
+      lastHash,
+      nonce,
+      timestamp,
     });
-    return block;
   }
 
-  static hash(timestamp: Date, lastHash: string, data: any): string {
-    return Sha256(`${timestamp}${lastHash}${data}`).toString();
+  static adjustDifficulty(lastBlock: Block, currentTime: number): number {
+    let { difficulty } = lastBlock;
+    difficulty =
+      lastBlock.timestamp + MINE_RATE > currentTime
+        ? difficulty + 1
+        : difficulty - 1;
+    return difficulty;
+  }
+
+  static hash(
+    timestamp: number,
+    lastHash: string,
+    data: any,
+    nonce: number,
+    difficulty: number,
+  ): string {
+    return Sha256(
+      `${timestamp}${lastHash}${data}${nonce}${difficulty}`,
+    ).toString();
   }
 
   public toString(): string | null {
@@ -57,11 +110,14 @@ export default class Block {
     Timestamp : ${this.timestamp}
     Last Hash : ${this.lastHash.substring(0, 10)}
     Hash      : ${this.hash.substring(0, 10)}
-    Data      : ${this.data}`;
+    Data      : ${this.data}
+    Nonce     : ${this.nonce}
+    Difficulty: ${this.difficulty};
+    `;
   }
 
   static blockHash(block: Block): string {
-    const { timestamp, lastHash, data } = block;
-    return Block.hash(timestamp, lastHash, data);
+    const { timestamp, lastHash, data, difficulty, nonce } = block;
+    return Block.hash(timestamp, lastHash, data, difficulty, nonce);
   }
 }
